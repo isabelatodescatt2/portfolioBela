b8678a9c829877331567f5771d7e42d2f731864f
Isabela Todescatt <87498826+isabelatodescatt2@users.noreply.github.com>
2025-03-23 21:03:16 -0300

    Add files via upload

---
+++ inicio/index.js
@@ -0,0 +1,4 @@
+document.getElementById('menuOpcoesSobreMim').addEventListener('click', function() {
+    const divSobreMim = document.getElementById('sobreMim');
+    divSobreMim.scrollIntoView({ behavior: 'smooth', block: 'start' });
+});
